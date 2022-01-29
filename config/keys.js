d22f1ed7f2909d1c9ad69f5f921c59825c4982bb
unknown <mpendu2016@gmail.com>
2022-01-29 12:38:17 +0200

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    MongoURI:'mongodb+srv://Thorne:Thorne@cluster0.9t4mj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MongoURI:'mongodb+srv://Mpendu:12345@cluster0.vpcvu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
